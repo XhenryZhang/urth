420f454ec4433c8fd0a7442b79a25d14416edd74
kyle <kyle_wong@ucsb.edu>
2020-12-09 15:58:22 -0800

    removed duplicate appbar

--- app/src/main/java/edu/ucsb/cs/cs184/urth/MapsActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/urth/MapsActivity.kt
@@ -38,7 +38,6 @@ class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
 //    override fun onCreateOptionsMenu(menu: Menu): Boolean {
